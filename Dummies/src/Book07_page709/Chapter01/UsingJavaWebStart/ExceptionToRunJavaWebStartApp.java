0564e99b1e7f6c4889a2f0afea23c8d00c9c8e42
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-02-14 09:58:16 +0000

    2023/02/14 (am)
    Book07 - Chapter01.
    Using Java Web Start - Complete.

---
+++ Dummies/src/Book07_page709/Chapter01/UsingJavaWebStart/ExceptionToRunJavaWebStartApp.java
@@ -0,0 +1,34 @@
+package Book07_page709.Chapter01.UsingJavaWebStart;
+
+public class ExceptionToRunJavaWebStartApp {
+	/*
+	In some cases, Java’s security settings may prevent a Java Web Start application
+	from running. If that happens, the user can correct the problem by adding the URL
+	of the application to the Java Exception Site List. Here are the steps for doing that
+	on a Windows computer:
+	 */
+
+	public static void main(String[] args) {
+		String one = "1. Open the Java Control Panel.\n\t" +
+					 "The easiest way to do that is to press the Start button, type java, press Enter,\n\t" +
+					 "and then click Configure Java.\n\t" +
+					 "Figure 1-5 shows the Java Control Panel for Java 9 (previous versions are similar).\n\n";
+
+		String two = "2. Click the Web Settings tab.\n\t" +
+					 "Because you’re changing a security setting, you might think you’d need to click\n\t" +
+					 "the Security tab. But the Exception Site List is found on the Web Settings tab, as\n\t" +
+					 "shown in Figure 1-6.\n\n";
+
+		String three = "3. Click Add.\n\t" +
+					   "This pops up a text entry box in which you can enter a URL.\n\n";
+
+		String four = "4. Type the URL of the web site you want to add and press Enter.\n\t" +
+				"In this example, type http://www.lowewriter.com. The URL will be added to\n\t" +
+				"the exception list.\n\n";
+
+		String five = "5. Click OK to close the Java Control Panel.\n\t" +
+				"You should now be able to run the application via Java Web Start.";
+
+		System.out.println(one + " " + two + " " + three + " " + four + " " + five);
+	}
+}
---
+++ Dummies/src/Book07_page709/Chapter01/UsingJavaWebStart/LaunchingClickMeUsingJavaWebstart.java
@@ -0,0 +1,20 @@
+package Book07_page709.Chapter01.UsingJavaWebStart;
+
+public class LaunchingClickMeUsingJavaWebstart {
+	/*
+	When all the files are in place, a web user can access the ClickMe application
+	by opening a web browser and navigating to the URL for the HTML page (in
+	our example, http://www.lowewriter.com/ClickMe/ClickMe.html). Refer to
+	Figure 1-2 to see how the page appears in the browser window.
+
+	When the user clicks the Run the ClickMe application link, the dialog box shown
+	in Figure 1-3 appears, asking the user whether she wants to run the application.
+	 */
+
+	public static void main(String[] args) {
+		String Run = "Assuming the user clicks Run, the ClickMe application is then run, as shown in\n" +
+				"Figure 1-4.";
+
+		System.out.println(Run);
+	}
+}
