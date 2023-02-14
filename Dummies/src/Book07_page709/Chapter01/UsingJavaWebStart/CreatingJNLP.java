b992d3777a08b2eed2766a599cef01bada705b3f
CnrdMr3 <123389745+CnrdMr3@users.noreply.github.com>
2023-02-14 09:10:34 +0000

    2023/02/14 (am)
    Book07 - Chapter01 - page715.
    Launching a Java App with HTML.

---
+++ Dummies/src/Book07_page709/Chapter01/UsingJavaWebStart/CreatingJNLP.java
@@ -0,0 +1,46 @@
+package Book07_page709.Chapter01.UsingJavaWebStart;
+
+public class CreatingJNLP {
+	/*
+	To deploy a Java application via Java Web Start, you must create a JNLP file that
+	contains the details needed to locate and run the application. The JNLP file is a
+	small XML file that contains information such as the name of the application, a
+	URL that points to the application’s JAR file, the vendor who created the application,
+	the Java version the application requires, and a URL that can be used to
+	download the Java runtime in case it isn’t already installed on the user’s computer.
+
+	JNLP stands for Java Network Launch Protocol, but that won’t be on the test.
+
+	If you aren’t already familiar with the basics of XML, you can learn about XML’s
+	syntax requirements in Book 8, Chapter 5.
+
+	Listing 1-2 shows a JNLP file named ClickMe.jnlp that can be used to run the
+	ClickMe application from the website www.lowewriter.com. for the ClickMe
+	application.
+	 */
+
+	public static void main(String[] args) {
+		String JNLP = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+				"<jnlp spec=\"1.0+\"\n" +
+				" codebase=\"http://www.lowewriter.com/ClickMe\" →3\n" +
+				" href=\"ClickMe.jnlp\"> →4\n" +
+				" <information>\n" +
+				" <title>ClickMe</title> →6\n" +
+				" <vendor>LoweWriter</vendor> →7\n" +
+				" <offline-allowed/>\n" +
+				" </information>\n" +
+				" <resources>\n" +
+				" <!-- Application Resources -->\n" +
+				" <j2se version=\"1.6+\"\n" +
+				" href=\"http://www.oracle.com/technetwork/java/javase/downloads\"/>\n" +
+				" <jar href=\"ClickMe.jar\" →14\n" +
+				" main=\"true\" />\n" +
+				" </resources>\n" +
+				" <application-desc main-class=\"ClickMe\"> →17\n" +
+				" </application-desc>\n" +
+				" <update check=\"background\"/>\n" +
+				"</jnlp>";
+
+		System.out.println(JNLP);
+	}
+}
---
+++ Dummies/src/Book07_page709/Chapter01/UsingJavaWebStart/HtmlFileToLaunchJavaApp.java
@@ -0,0 +1,32 @@
+package Book07_page709.Chapter01.UsingJavaWebStart;
+
+public class HtmlFileToLaunchJavaApp {
+	/*
+	Once you’ve created a JNLP file that describes your Java Web Start application, you
+	can create an HTML file that your users can view to launch the application. The
+	HTML file should have a link to the JNPL file. The easiest way to provide that link
+	is to use a element in the HTML file, as shown in the ClickMe.html file shown
+	in Listing 1-3.
+	 */
+
+	public static void main(String[] args) {
+		// An HTML File That Links to a JNPL File
+		String HTML = "<html>\n" +
+				" <head>\n\t" +
+				" <title>ClickMe (Java Web Start)</title>\n" +
+				" </head>\n\t" +
+				" <body>\n\t\t" +
+				" <h1>ClickMe!</h1>\n\t\t\t" +
+				" <a href=ClickMe.jnlp>Run the ClickMe application</a>\n\t\t" +
+				" </body>\n" +
+				"</html>\n";
+
+		System.out.println(HTML);
+
+		/*
+		In Listing 1-3, the element provides the necessary link:
+		>> <a href-clickme.jnlp>Run the ClickMe application</a>
+		Figure 1-2 shows how the ClickMe.html file appears when displayed in a browser window.
+		 */
+	}
+}
